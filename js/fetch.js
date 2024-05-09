d4edef70b85c467cff9eb0552bbf2f479a8f89b4
HammiAhlan <hammiahlan@gmail.com>
2024-05-10 04:02:43 +0700

    push

---
+++ js/fetch.js
@@ -0,0 +1,6 @@
+import { get } from "https://bukulapak.github.io/api/process.js"; 
+let urlAPI = "https://uts-webservice2024-172a0582e09a.herokuapp.com";
+get(urlAPI,isiTableDonasi);
+function isiTableDonasi(results){
+    console.log(results);
+}
