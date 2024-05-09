12165ef00e3d595b619cc304f00f4d3f07f706e6
HammiAhlan <hammiahlan@gmail.com>
2024-05-10 04:32:45 +0700

    whatasp

--- js/fetch.js
+++ js/fetch.js
@@ -1,5 +1,5 @@
 import { get } from "https://bukulapak.github.io/api/process.js"; 
-let urlAPI = "https://uts-webservice2024-172a0582e09a.herokuapp.com";
+let urlAPI = "https://uts-webservice2024-172a0582e09a.herokuapp.com/donasi";
 get(urlAPI,isiTableDonasi);
 function isiTableDonasi(results){
     console.log(results);
