f44d70eccf31b1a124cb128ef108cde4b12c44d5
HammiAhlan <hammiahlan@gmail.com>
2024-05-10 12:42:27 +0700

    Harus

---
+++ js/table.js
@@ -0,0 +1,27 @@
+<div class="col-lg-6 order-first order-lg-last">
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">ID</th>
+                                            <th scope="col">Nama</th>
+                                            <th scope="col">Nohp</th>
+                                            <th scope="col">jenis donasi</th>
+                                            <th scope="col">jumlah</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <th scope="row">1</th>
+                                            <td>jenis</td>
+                                            <td>lokasi</td>
+                                            
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">2</th>
+                                            <td>Jane Smith</td>
+                                            <td>$50</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+
