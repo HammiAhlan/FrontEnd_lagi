eceef64b66719a0f8c7ccfc9fc817d3b0d0fc916
HammiAhlan <hammiahlan@gmail.com>
2024-05-10 10:05:12 +0700

    haduh

--- js/fetch.js
+++ js/fetch.js
@@ -1,7 +1,13 @@
-import { get } from "https://bukulapak.github.io/api/process.js"; 
+import { get } from "https://bukulapak.github.io/api/process.js";
+import { setInner } from "https://bukulapak.github.io/element/process.js"; 
 let urlAPI = "https://uts-webservice2024-172a0582e09a.herokuapp.com/donasi";
 get(urlAPI,isiTablePresensi);
 function isiTablePresensi(results){
     console.log(results);
+    results.forEach(isiRow);
 }
-//bodo
+function isiRow(value){
+    console.log(value)
+}
+
+
