16049dfbc4b6e0f36a0c21c16e227ab125dec1bf
HammiAhlan <hammiahlan@gmail.com>
2024-05-10 09:59:27 +0700

    hammo

--- js/config/url.js
+++ js/config/url.js
@@ -1,1 +1,1 @@
-export let urlAPI = "https://uts-webservice2024-172a0582e09a.herokuapp.com/donasi";
+export let urlAPI = "https://uts-webservice2024-172a0582e09a.herokuapp.com//donasi";
--- js/fetch.js
+++ js/fetch.js
@@ -1,7 +1,7 @@
 import { get } from "https://bukulapak.github.io/api/process.js"; 
 let urlAPI = "https://uts-webservice2024-172a0582e09a.herokuapp.com/donasi";
-get(urlAPI,isiTableDonasi);
-function isiTableDonasi(results){
+get(urlAPI,isiTablePresensi);
+function isiTablePresensi(results){
     console.log(results);
 }
 //bodo
